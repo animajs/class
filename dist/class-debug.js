aa655b167b0625b99c93e286a0a98ecce9ee8087
chencheng <sorrycc@gmail.com>
2014-03-31 15:53:12 +0800

    update family name and rebuild.

--- dist/class-debug.js
+++ dist/class-debug.js
@@ -1,4 +1,4 @@
-define("arale/class/1.1.0/class-debug", [], function(require, exports, module) {
+define("anima/class/2.0.0/class-debug", [], function(require, exports, module) {
     // Class
     // -----------------
     // Thanks to:
@@ -11,138 +11,85 @@ define("arale/class/1.1.0/class-debug", [], function(require, exports, module) {
     // The base Class implementation.
     function Class(o) {
         // Convert existed function to Class.
-        if (!(this instanceof Class) && isFunction(o)) {
-            return classify(o);
+        if (isFunction(o)) {
+            // extended object will not call this function but call initialize
+            return classify(o).implement({
+                initialize: o
+            });
         }
     }
     module.exports = Class;
     // Create a new Class.
     //
-    //  var SuperPig = Class.create({
-    //    Extends: Animal,
-    //    Implements: Flyable,
+    //  var Animal = Class.create({
+    //    initialize: function() {}
+    //  });
+    //
+    //  var SuperPig = Animal.extend({
     //    initialize: function() {
     //      SuperPig.superclass.initialize.apply(this, arguments)
-    //    },
-    //    Statics: {
-    //      COLOR: 'red'
     //    }
-    // })
-    //
-    Class.create = function(parent, properties) {
-        if (!isFunction(parent)) {
-            properties = parent;
-            parent = null;
-        }
+    //  }).implement(Flyable)
+    Class.create = function(properties) {
         properties || (properties = {});
-        parent || (parent = properties.Extends || Class);
-        properties.Extends = parent;
         // The created class constructor
         function SubClass() {
-            // Call the parent constructor.
-            parent.apply(this, arguments);
             // Only call initialize in self constructor.
             if (this.constructor === SubClass && this.initialize) {
                 this.initialize.apply(this, arguments);
             }
         }
-        // Inherit class (static) properties from parent.
-        if (parent !== Class) {
-            mix(SubClass, parent, parent.StaticsWhiteList);
-        }
+        // Transform normal function to class
+        classify(SubClass);
         // Add instance properties to the subclass.
-        implement.call(SubClass, properties);
-        // Make subclass extendable.
-        return classify(SubClass);
-    };
-    function implement(properties) {
-        var key, value;
-        for (key in properties) {
-            value = properties[key];
-            if (Class.Mutators.hasOwnProperty(key)) {
-                Class.Mutators[key].call(this, value);
-            } else {
-                this.prototype[key] = value;
-            }
-        }
-    }
-    // Create a sub Class based on `Class`.
-    Class.extend = function(properties) {
-        properties || (properties = {});
-        properties.Extends = this;
-        return Class.create(properties);
+        SubClass.implement(properties);
+        return SubClass;
     };
     function classify(cls) {
-        cls.extend = Class.extend;
+        _extend(cls, Class);
+        cls.extend = extend;
         cls.implement = implement;
         return cls;
     }
-    // Mutators define special properties.
-    Class.Mutators = {
-        Extends: function(parent) {
-            var existed = this.prototype;
-            var proto = createProto(parent.prototype);
-            // Keep existed properties.
-            mix(proto, existed);
-            // Enforce the constructor to be what we expect.
-            proto.constructor = this;
-            // Set the prototype chain to inherit from `parent`.
-            this.prototype = proto;
-            // Set a convenience property in case the parent's prototype is
-            // needed later.
-            this.superclass = parent.prototype;
-        },
-        Implements: function(items) {
-            isArray(items) || (items = [ items ]);
-            var proto = this.prototype, item;
-            while (item = items.shift()) {
-                mix(proto, item.prototype || item);
-            }
-        },
-        Statics: function(staticProperties) {
-            mix(this, staticProperties);
+    function implement(properties) {
+        Array.isArray(properties) || (properties = [ properties ]);
+        var item, proto = this.prototype;
+        while (item = properties.shift()) {
+            mix(proto, item.prototype || item);
         }
-    };
-    // Shared empty constructor function to aid in prototype-chain creation.
-    function Ctor() {}
-    // See: http://jsperf.com/object-create-vs-new-ctor
-    var createProto = Object.__proto__ ? function(proto) {
-        return {
-            __proto__: proto
-        };
-    } : function(proto) {
-        Ctor.prototype = proto;
-        return new Ctor();
-    };
+        return this;
+    }
+    function extend(properties) {
+        var subClass = Class.create(properties);
+        _extend(subClass, this);
+        mix(subClass, this, [ "extend", "implement", "superclass" ]);
+        return subClass;
+    }
+    function _extend(self, parent) {
+        var existed = self.prototype;
+        var proto = Object.create(parent.prototype);
+        // Keep existed properties.
+        mix(proto, existed);
+        // Enforce the constructor to be what we expect.
+        proto.constructor = self;
+        // Set the prototype chain to inherit from `parent`.
+        self.prototype = proto;
+        // Set a convenience property in case the parent's prototype is
+        // needed later.
+        self.superclass = parent.prototype;
+    }
     // Helpers
     // ------------
-    function mix(r, s, wl) {
+    function mix(r, s, bl) {
         // Copy "all" properties including inherited ones.
         for (var p in s) {
             if (s.hasOwnProperty(p)) {
-                if (wl && indexOf(wl, p) === -1) continue;
-                // 在 iPhone 1 代等设备的 Safari 中，prototype 也会被枚举出来，需排除
-                if (p !== "prototype") {
-                    r[p] = s[p];
-                }
+                if (bl && bl.indexOf(p) !== -1) continue;
+                r[p] = s[p];
             }
         }
     }
-    var toString = Object.prototype.toString;
-    var isArray = Array.isArray || function(val) {
-        return toString.call(val) === "[object Array]";
-    };
-    var isFunction = function(val) {
-        return toString.call(val) === "[object Function]";
-    };
-    var indexOf = Array.prototype.indexOf ? function(arr, item) {
-        return arr.indexOf(item);
-    } : function(arr, item) {
-        for (var i = 0, len = arr.length; i < len; i++) {
-            if (arr[i] === item) {
-                return i;
-            }
-        }
-        return -1;
-    };
+    function isFunction(val) {
+        return Object.prototype.toString.call(val) === "[object Function]";
+    }
 });
