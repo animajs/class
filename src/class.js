e3a06f9bb3c784b76084b21872a1306915f13520
chencheng <sorrycc@gmail.com>
2014-05-08 15:09:09 +0800

    transform to commonjs

--- src/class.js
+++ src/class.js
@@ -1,120 +1,117 @@
-define(function(require, exports, module) {
 
-  // Class
-  // -----------------
-  // Thanks to:
-  //  - http://mootools.net/docs/core/Class/Class
-  //  - http://ejohn.org/blog/simple-javascript-inheritance/
-  //  - https://github.com/ded/klass
-  //  - http://documentcloud.github.com/backbone/#Model-extend
-  //  - https://github.com/joyent/node/blob/master/lib/util.js
-  //  - https://github.com/kissyteam/kissy/blob/master/src/seed/src/kissy.js
+// Class
+// -----------------
+// Thanks to:
+//  - http://mootools.net/docs/core/Class/Class
+//  - http://ejohn.org/blog/simple-javascript-inheritance/
+//  - https://github.com/ded/klass
+//  - http://documentcloud.github.com/backbone/#Model-extend
+//  - https://github.com/joyent/node/blob/master/lib/util.js
+//  - https://github.com/kissyteam/kissy/blob/master/src/seed/src/kissy.js
 
 
-  // The base Class implementation.
-  function Class(o) {
-    // Convert existed function to Class.
-    if (isFunction(o)) {
-      // extended object will not call this function but call initialize
-      return classify(o).implement({
-        initialize: o
-      });
+// The base Class implementation.
+function Class(o) {
+  // Convert existed function to Class.
+  if (isFunction(o)) {
+    // extended object will not call this function but call initialize
+    return classify(o).implement({
+      initialize: o
+    });
+  }
+}
+
+module.exports = Class;
+
+
+// Create a new Class.
+//
+//  var Animal = Class.create({
+//    initialize: function() {}
+//  });
+//
+//  var SuperPig = Animal.extend({
+//    initialize: function() {
+//      SuperPig.superclass.initialize.apply(this, arguments)
+//    }
+//  }).implement(Flyable)
+
+Class.create = function(properties) {
+  properties || (properties = {});
+
+  // The created class constructor
+  function SubClass() {
+    // Only call initialize in self constructor.
+    if (this.constructor === SubClass && this.initialize) {
+      this.initialize.apply(this, arguments);
     }
   }
 
-  module.exports = Class;
+  // Transform normal function to class
+  classify(SubClass);
 
+  // Add instance properties to the subclass.
+  SubClass.implement(properties);
 
-  // Create a new Class.
-  //
-  //  var Animal = Class.create({
-  //    initialize: function() {}
-  //  });
-  //
-  //  var SuperPig = Animal.extend({
-  //    initialize: function() {
-  //      SuperPig.superclass.initialize.apply(this, arguments)
-  //    }
-  //  }).implement(Flyable)
+  return SubClass;
+};
 
-  Class.create = function(properties) {
-    properties || (properties = {});
+function classify(cls) {
+  _extend(cls, Class);
+  cls.extend = extend;
+  cls.implement = implement;
+  return cls;
+}
 
-    // The created class constructor
-    function SubClass() {
-      // Only call initialize in self constructor.
-      if (this.constructor === SubClass && this.initialize) {
-        this.initialize.apply(this, arguments);
-      }
-    }
+function implement(properties) {
+  Array.isArray(properties) || (properties = [properties]);
+  var item, proto = this.prototype;
 
-    // Transform normal function to class
-    classify(SubClass);
-
-    // Add instance properties to the subclass.
-    SubClass.implement(properties);
-
-    return SubClass;
-  };
-
-  function classify(cls) {
-    _extend(cls, Class);
-    cls.extend = extend;
-    cls.implement = implement;
-    return cls;
+  while (item = properties.shift()) {
+    mix(proto, item.prototype || item);
   }
+  return this;
+}
 
-  function implement(properties) {
-    Array.isArray(properties) || (properties = [properties]);
-    var item, proto = this.prototype;
+function extend(properties) {
+  var subClass = Class.create(properties);
+  _extend(subClass, this);
+  mix(subClass, this, ['extend', 'implement', 'superclass']);
+  return subClass;
+}
 
-    while (item = properties.shift()) {
-      mix(proto, item.prototype || item);
-    }
-    return this;
-  }
+function _extend(self, parent) {
+  var existed = self.prototype;
+  var proto = Object.create(parent.prototype);
 
-  function extend(properties) {
-    var subClass = Class.create(properties);
-    _extend(subClass, this);
-    mix(subClass, this, ['extend', 'implement', 'superclass']);
-    return subClass;
-  }
+  // Keep existed properties.
+  mix(proto, existed);
 
-  function _extend(self, parent) {
-    var existed = self.prototype;
-    var proto = Object.create(parent.prototype);
+  // Enforce the constructor to be what we expect.
+  proto.constructor = self;
 
-    // Keep existed properties.
-    mix(proto, existed);
+  // Set the prototype chain to inherit from `parent`.
+  self.prototype = proto;
 
-    // Enforce the constructor to be what we expect.
-    proto.constructor = self;
-
-    // Set the prototype chain to inherit from `parent`.
-    self.prototype = proto;
-
-    // Set a convenience property in case the parent's prototype is
-    // needed later.
-    self.superclass = parent.prototype;
-  }
+  // Set a convenience property in case the parent's prototype is
+  // needed later.
+  self.superclass = parent.prototype;
+}
 
 
-  // Helpers
-  // ------------
+// Helpers
+// ------------
 
-  function mix(r, s, bl) {
-    // Copy "all" properties including inherited ones.
-    for (var p in s) {
-      if (s.hasOwnProperty(p)) {
-        if (bl && bl.indexOf(p) !== -1) continue;
-        r[p] = s[p];
-      }
+function mix(r, s, bl) {
+  // Copy "all" properties including inherited ones.
+  for (var p in s) {
+    if (s.hasOwnProperty(p)) {
+      if (bl && bl.indexOf(p) !== -1) continue;
+      r[p] = s[p];
     }
   }
+}
 
-  function isFunction(val) {
-    return Object.prototype.toString.call(val) === '[object Function]';
-  }
-
-});
+function isFunction(val) {
+  return Object.prototype.toString.call(val) === '[object Function]';
+}
